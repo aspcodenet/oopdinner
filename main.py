7c1f2d995f7c8a059a9e983605250867aad6ac61
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-08-30 11:22:59 +0200

    qwqwwe

--- main.py
+++ main.py
@@ -1,5 +1,75 @@
 import random
 
+class OurQueue:
+    def __init__(self):
+        self.__listan = []
+    def Add(self, p):
+        self.__listan.append(p)
+    def GetNext(self):
+        if len(self.__listan) == 0:
+            return None
+        item = self.__listan[0]    
+        del self.__listan[0]
+        return item
+
+
+class OurStack:
+    def __init__(self):
+        self.__listan = []
+    def Add(self, p):
+        self.__listan.append(p)
+    def GetNext(self):
+        if len(self.__listan) == 0:
+            return None
+        item = self.__listan[len(self.__listan)-1]    
+        del self.__listan[len(self.__listan)-1]
+        return item
+
+    
+
+
+class Patient:
+    def __init__(self, namn:str):
+        self.Namn = namn
+
+queuen = []
+
+def NewPatient():
+    namn = input("Vad heter patienten:")
+    p = Patient(namn)
+    queuen.append(p)
+
+def RopaUppNasta():
+    a = 123
+    b = 2313
+    patienten = queuen[0]    
+    print("Nästa = " + patienten.Namn)
+    del queuen[0]
+
+
+
+k = OurQueue()  # class OutQueue ska ni skapa
+k.Add(Patient("Stefan"))
+k.Add(Patient("kalle"))
+k.Add(Patient("lisa"))
+p = k.GetNext() # p.Namn = "Stefan"
+
+k = OurStack()  # class OurStack ska ni skapa
+k.Add(Patient("Stefan"))
+k.Add(Patient("kalle"))
+k.Add(Patient("lisa"))
+p = k.GetNext() # p.namn = "lisa"
+
+
+
+
+
+NewPatient()    
+NewPatient()
+NewPatient()
+while True:
+    RopaUppNasta()
+
 class GameCharacter:
     def __init__(self, namn:str):
         self.Namn = namn
