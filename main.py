e771331e575706264dbb9f1e03225937d8b7a171
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-08-30 13:50:43 +0200

    aaa

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import random
 
 class OurQueue:
@@ -21,7 +22,7 @@ class OurStack:
 
     def Add(self, p):
         self.__listan.append(p)
-        
+
     def GetNext(self):
         if len(self.__listan) == 0:
             return None
@@ -32,16 +33,60 @@ class OurStack:
     
 
 
+class Patient22:
+    def __init__(self, id:int, namn:str, age:int):
+        self.__id = id
+        self.__namn = namn
+        self._age = age
+    
+    def getNamn(self):
+        return self.__namn
+
+    def setNamn(self, newValue):
+        if len(newValue) < 2:
+            print("För kort")
+        else:
+            self.__namn = newValue
+
+    
+    @property        
+    def Age(self):
+        return self._age
+    
+    @Age.setter
+    def Age(self, newValue):
+        if newValue > 0 and newValue < 150:
+            self._age = newValue
+
+
+p3 = Patient22(12, "Stefan",-45)
+print(p3.setNamn("Stefan1"))
+print(p3.getNamn())
+#p3.setAge(23)
+p3.age = 23
+
+p3.age = p3.age  + 1
+#p3.setAge(p3.getAge() + 1)
+
+
+
+
+
+
+@dataclass
 class Patient:
-    def __init__(self, namn:str, age:int):
-        self.Namn = namn
-        self.Age = age
+    Id :int
+    Namn: str
+    Age: int
+    City: str
+    Postnr: str
 
-
-p = Patient("Stefan Holmberg", 50)
-p2 = Patient("Stefan Holmberg", 50)
-if p == p2:
+p = Patient(22, "Stefan Holmberg", 50, "ewer","11122")
+p2 = Patient(22, "Stefan Holmberg", 50, "ewer", "11122")
+if p == p2: # SKA BLI SANT
     print("Japp det är samma person") # todo add "Id"
+else:
+    print("Inte samma person")    
 
 
 queuen = []
